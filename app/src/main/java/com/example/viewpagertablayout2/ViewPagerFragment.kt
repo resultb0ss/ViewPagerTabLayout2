2295e0a2378f4aa7516ba218787bd9b92c77f691
Admin <reteamboss@gmail.com>
2024-10-17 12:28:03 +0300

    1

--- app/src/main/java/com/example/viewpagertablayout2/ViewPagerFragment.kt
+++ app/src/main/java/com/example/viewpagertablayout2/ViewPagerFragment.kt
@@ -1,16 +1,19 @@
 package com.example.viewpagertablayout2
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.example.viewpagertablayout2.databinding.FragmentViewPagerBinding
 
 class ViewPagerFragment : Fragment() {
 
     private lateinit var binding: FragmentViewPagerBinding
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,6 +22,9 @@ class ViewPagerFragment : Fragment() {
         binding = FragmentViewPagerBinding.inflate(inflater,container,false)
 
         val viewPagerItem = arguments?.getSerializable("page" ) as Page
+
+        binding.webView.webViewClient = WebViewClient()
+        binding.webView.settings.javaScriptEnabled = true
         binding.webView.loadUrl(viewPagerItem.url)
 
         return binding.root
